86d599d6f687b3cc83cbe9272faf546d81bc1087
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-05-30 23:33:43 +0100

    added hidden forum and js function to toggle when click on button add city

---
+++ main.js
@@ -0,0 +1,12 @@
+function showForm() {
+    var x = document.getElementById("new-city-content");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function search() {
+    
+}
