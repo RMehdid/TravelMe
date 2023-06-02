097656e8bf63df68426076efc622937ee0307b83
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-06-02 10:02:10 +0100

    worked on php api

--- script/main.js
+++ script/main.js
@@ -1,10 +1,10 @@
 function showForm() {
-    var x = document.getElementById("new-city-content");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  var x = document.getElementById("new-city-content");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
 
 function submitForm(event) {
@@ -12,7 +12,7 @@ function submitForm(event) {
 
   // Make an AJAX request to the PHP script
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       // Handle the response from the PHP script
       console.log(this.responseText);
@@ -22,6 +22,18 @@ function submitForm(event) {
   xhttp.send(new FormData(event.target));
 }
 
-function search() {
-    
+function toggleAddHotel() {
+  var element1 = document.getElementById("hotels");
+  var element2 = document.getElementById("hotels-new");
+
+  // Toggle the visibility of the elements
+  if (element1.style.display === "none") {
+    element1.style.display = "inline";
+    element2.style.display = "none";
+  } else {
+    element1.style.display = "none";
+    element2.style.display = "inline";
+  }
 }
+
+function search() {}
--- server/addCity.php
+++ server/addCity.php
@@ -1,42 +1,108 @@
 <?php
 
-require "connect.php";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-  connect();
-  function insertData() {
-    global $conn;
+// require "connect.php";
+function insertData()
+{
+  global $conn;
 
-    // Retrieve form field values
-    $ville = $_POST['ville'];
-    $pays = $_POST['pays'];
-    $continent = $_POST['continent'];
-    $descriptif = $_POST['descriptif'];
-    $sites = $_POST['sites'];
-    $hotels = $_POST['hotels'];
-    $restaurants = $_POST['restaurants'];
-    $gares = $_POST['gares'];
-    $aeroports = $_POST['aeroports'];
+  $db_host = 'localhost';
+    $db_user = 'root';
+    $db_password = 'root';
+    $db_db = 'voyage';
 
-    echo $_POST['hotels'];
+    $conn= @new mysqli(
+        $db_host,
+        $db_user,
+        $db_password,
+        $db_db
+    );
 
-    // Prepare and execute the SQL query
-    $stmt = $conn->prepare("INSERT INTO ville (ville, pays, continent, descriptif, sites, hotels, restaurants, gares, aeroports) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssssssss", $ville, $pays, $continent, $descriptif, $sites, $hotels, $restaurants, $gares, $aeroports);
-    $stmt->execute();
+  // Retrieve form field values
+  $ville = $_POST['ville'];
+  $pays = $_POST['pays'];
+  $continent = $_POST['continent'];
+  $descriptif = $_POST['descriptif'];
+  $sites = $_POST['sites'];
+  $photos = $_POST['photos'];
+  $hotels = $_POST['hotels'];
+  $restaurants = $_POST['restaurants'];
+  $gares = $_POST['gares'];
+  $aeroports = $_POST['aeroports'];
 
-    // Check if the insertion was successful
-    if ($stmt->affected_rows > 0) {
-        echo "Data inserted successfully.";
-    } else {
-        echo "Error inserting data.";
+
+  $stmt = $conn->prepare("INSERT INTO continent (nomcon) VALUES (?)");
+  $stmt->bind_param("s", $continent);
+  $stmt->execute();
+
+  $idcon = $stmt->insert_id;
+
+  $stmt = $conn->prepare("INSERT INTO pays (nompay, idcon) VALUES (?, ?)");
+  $stmt->bind_param("ss", $pays, $idcon);
+  $stmt->execute();
+
+  $idpay = $stmt->insert_id;
+  // Prepare and execute the SQL query
+  $stmt = $conn->prepare("INSERT INTO ville (nomvil, idpay, descvil) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $ville, $idpay, $descriptif);
+  $stmt->execute();
+
+  // Retrieve the last inserted ID from 'ville' table
+  $idvil = $stmt->insert_id;
+
+  $stmt = $conn->prepare("INSERT INTO site (nomsit, cheminphoto, idvil) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $sites, $photos, $idvil);
+  $stmt->execute();
+
+  // Check if any of the necessary fields are filled
+  if (!empty($hotels) || !empty($restaurants) || !empty($gares) || !empty($aeroports)) {
+    // Prepare and execute the SQL query for inserting into 'necessaire' table
+    $stmt = $conn->prepare("INSERT INTO necessaire (idvil, typenec, nomnec) VALUES (?, ?, ?)");
+
+    // Insert 'hotels' into 'necessaire' table
+    if (!empty($hotels)) {
+      $type = 'hotel';
+      $stmt->bind_param("sss", $idvil, $type, $hotels);
+      $stmt->execute();
     }
 
-    // Close the statement
-    $stmt->close();
+    // Insert 'restaurants' into 'necessaire' table
+    if (!empty($restaurants)) {
+      $type = 'restaurants';
+      $stmt->bind_param("sss", $idvil, $type, $restaurants);
+      $stmt->execute();
+    }
 
-    // Close the database connection
-    $conn->close();
+    // Insert 'gares' into 'necessaire' table
+    if (!empty($gares)) {
+      $type = 'gares';
+      $stmt->bind_param("sss", $idvil, $type, $gares);
+      $stmt->execute();
+    }
+
+    // Insert 'aeroports' into 'necessaire' table
+    if (!empty($aeroports)) {
+      $type = 'aeroports';
+      $stmt->bind_param("sss", $idvil, $type, $aeroports);
+      $stmt->execute();
+    }
+  }
+
+  // Check if the insertion was successful
+  if ($stmt->affected_rows > 0) {
+    echo "Data inserted successfully.";
+  } else {
+    echo "Error inserting data.";
+  }
+
+  // Close the statement
+  $stmt->close();
+
+  // Close the database connection
+  $conn->close();
 }
 
-  $mysqli->close();
+insertData();
 ?>
